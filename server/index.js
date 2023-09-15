6d05e1e34f493986b25726651496aec5c9c789d6
Emidio Valeretto <53981181+emidiovaleretto@users.noreply.github.com>
2023-09-15 21:22:46 +0000

    chore: retrieve the transcribed audio and pass it to the response

--- server/index.js
+++ server/index.js
@@ -2,15 +2,15 @@ import cors from "cors"
 import express from "express"
 
 import { download } from "./downloader.js"
+import { transcribeAudio } from "./transcribe.js"
 
 const app = express()
 app.use(cors())
 
-app.get("/summary/v=:id", (request, response) => {
-  let videoId = request.params.id
-  download(videoId)
-
-  response.json({ result: "Download completed successfully" })
+app.get("/summary/v=:id", async (request, response) => {
+  await download(request.params.id)
+  const result = await transcribeAudio()
+  response.json({ result })
 })
 
 app.listen(3333, () => console.log("Server running on port 3333"))
