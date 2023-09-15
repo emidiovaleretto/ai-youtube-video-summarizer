8b2762239b27b0887e548801b03b1a4ab6d835ff
Emidio Valeretto <53981181+emidiovaleretto@users.noreply.github.com>
2023-09-15 21:24:10 +0000

    chore: create a transcribeAudio function

---
+++ server/transcribe.js
@@ -0,0 +1,5 @@
+import { transcriptionExample } from "./utils/transcription.js"
+
+export async function transcribeAudio() {
+  return transcriptionExample
+}
---
+++ server/utils/transcription.js
@@ -0,0 +1,3 @@
+export const transcriptionExample = `
+JavaScript is a programming language that conforms to the ECMAScript specification. JavaScript is high-level, often just-in-time compiled, and multi-paradigm. It has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions. Alongside HTML and CSS, JavaScript is one of the core technologies of the World Wide Web. JavaScript enables interactive web pages and is an essential part of web applications. The vast majority of websites use it for client-side page behavior, and all major web browsers have a dedicated JavaScript engine to execute it.
+`
