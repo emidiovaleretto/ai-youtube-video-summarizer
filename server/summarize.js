3baad0d4a3581f85d5505d392f920dde3c43b1db
Emidio Valeretto <53981181+emidiovaleretto@users.noreply.github.com>
2023-09-15 21:24:32 +0000

    chore: create a summarizeText function

---
+++ server/summarize.js
@@ -0,0 +1,5 @@
+import { summaryExample } from "./utils/summary.js"
+
+export async function summarizeText(text) {
+  return summaryExample
+}
---
+++ server/utils/summary.js
@@ -0,0 +1,3 @@
+export const summaryExample = `
+JavaScript, often abbreviated as JS, is a programming language that conforms to the ECMAScript specification. JavaScript is high-level, often just-in-time compiled, and multi-paradigm. It has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions.
+`
