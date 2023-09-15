4a90bfefe9e43426333442f8890eaeb06e581b8d
Emidio Valeretto <53981181+emidiovaleretto@users.noreply.github.com>
2023-09-15 21:23:33 +0000

    chore: include a new Promise to handle the resolve and reject events

--- server/downloader.js
+++ server/downloader.js
@@ -1,26 +1,29 @@
 import ytdl from "ytdl-core"
 import fs from "fs"
 
-export const download = (videoId) => {
-  let url = `https://www.youtube.com/watch?v=${videoId}`
-  let output = `./tmp/${videoId}.mp4`
+export const download = (videoId) =>
+  new Promise((resolve, reject) => {
+    let url = `https://www.youtube.com/watch?v=${videoId}`
+    let output = `./tmp/${videoId}.mp4`
 
-  console.log(`Downloading video: ${videoId}`)
+    console.log(`Downloading video: ${videoId}`)
 
-  ytdl(url, { quality: "lowestaudio", filter: "audioonly" })
-    .on("info", (info) => {
-      const seconds = info.formats[0].approxDurationMs / 1000
+    ytdl(url, { quality: "lowestaudio", filter: "audioonly" })
+      .on("info", (info) => {
+        const seconds = info.formats[0].approxDurationMs / 1000
 
-      if (seconds < 60) {
-        throw new Error("Video is less than 1 minute long.")
-      }
-      return
-    })
-    .on("end", () => {
-      console.log("Finished downloading")
-    })
-    .on("error", (error) => {
-      console.log(`Unable to download video: ${error}`)
-    })
-    .pipe(fs.createWriteStream(output))
-}
+        if (seconds < 60) {
+          throw new Error("Video is less than 1 minute long.")
+        }
+        return
+      })
+      .on("end", () => {
+        console.log("Finished downloading")
+        resolve()
+      })
+      .on("error", (error) => {
+        console.log(`Unable to download video: ${error}`)
+        reject(error)
+      })
+      .pipe(fs.createWriteStream(output))
+  })
