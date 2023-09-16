8efe6752787ff5281512fff2d43c3d69b6bc5bb1
Emidio Valereto <emidio.valereto@gmail.com>
2023-09-16 20:14:52 +0100

    chore: add an event to fire the loading class

--- web/form.js
+++ web/form.js
@@ -1,30 +1,38 @@
-import { server } from "./server.js"
+import { server } from './server.js'
 
-const form = document.querySelector("#form")
-const input = document.querySelector("#url-input")
-const content = document.querySelector("#content")
-const error = document.querySelector(".error")
+const form = document.querySelector('#form')
+const input = document.querySelector('#url-input')
+const button = document.querySelector('.input-submit button')
+const content = document.querySelector('#content')
+const error = document.querySelector('.error')
+const loading = document.querySelector('.overlay')
 
-form.addEventListener("submit", async (event) => {
+form.addEventListener('submit', async (event) => {
   event.preventDefault()
+
+  content.classList.add('placeholder')
 
   const url = input.value
 
-  if (!url.includes("watch?v=")) {
-    error.classList.remove("hidden")
-    input.style.border = "1px solid red"
+  if (!url.includes('watch?v=')) {
+    error.classList.remove('hidden')
+    input.style.border = '1px solid red'
     return
   }
 
-  const [path, id] = url.split("=")
+  const [path, id] = url.split('=')
 
-  error.classList.add("hidden")
-  input.style.border = "1px solid #ccc"
-  content.textContent = "Getting video content..."
+  error.classList.add('hidden')
+  input.style.border = '1px solid #ccc'
+  content.textContent = 'Getting video content...'
+  button.classList.add('disabled')
+  loading.classList.remove('hidden')
 
   const transcription = await server.get(`/summary/v=${id}`)
 
-  console.log(transcription.data.result)
-
   content.textContent = transcription.data.result
+
+  content.classList.remove('placeholder')
+  button.classList.remove('disabled')
+  loading.classList.add('hidden')
 })
